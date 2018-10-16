8f98c410a4b077df78e84161d5f0b63f6b5eb33e
maliaoMJ <1302151931@qq.com>
2018-10-16 13:32:00 +0800

    添加sass&&封装store

---
+++ components/index/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="VueToNuxtLogo">
+    header compontent
+  </div>
+</template>
+
+<style lang="scss">
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,30 @@
 <template>
-  <div>nuxt.js test</div>
+  <div class="container">
+    明若清渊
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+const HomeKey = 'Home'
 export default {
-  components: {
-    Logo
-  }
+  components: {},
+  created: function() {
+    console.log(this.$store.state.modules[HomeKey].name)
+  },
+  methods: {}
 }
 </script>
 
 <style>
 .container {
   min-height: 100vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background: powderblue;
+  color: #fff;
+  font-size: 27px;
 }
 </style>
